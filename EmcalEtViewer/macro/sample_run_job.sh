863e464475533f219e66886c6a61225d924161b8
Ryotaro Koike <ryotaro@sphnxuser04.sdcc.bnl.gov>
2026-06-10 03:39:58 -0400

    sorted out git-tracked files

--- EmcalEtViewer/macro/sample_run_job.sh
+++ EmcalEtViewer/macro/sample_run_job.sh
@@ -5,7 +5,7 @@
 
 #source /opt/sphenix/core/bin/sphenix_setup.sh -n ana.464
 source /opt/sphenix/core/bin/sphenix_setup.sh -n ana
-# source /opt/sphenix/core/bin/sphenix_setup.sh -n ana.532
+# source /opt/sphenix/core/bin/sphenix_setup.sh -n ana.504
 
 
 export MYINSTALL=/sphenix/u/ryotaro/DirectPhotonAnalysis/EmcalEtViewer/install
@@ -15,15 +15,18 @@ export ROOT_INCLUDE_PATH=$MYINSTALL/include:$ROOT_INCLUDE_PATH
 
 source /opt/sphenix/core/bin/setup_local.sh $MYINSTALL
 
+echo $ROOT_INCLUDE_PATH | sed -e "s/:/\n/g"
 
 process=$1
-nEvents=$2
-save_tree=$3
+runnumber=$2
+nEvents=$3
+save_tree=$4
 
 echo process: ${process}
+echo runnumber: ${runnumber}
 echo nEvents: ${nEvents}
 echo save_tree: ${save_tree}
 
-root.exe -q -b Fun4All_SingleParticlePi0.C\(${process},${nEvents},${save_tree}\)
+root.exe -q -b Fun4All_DisplayLegoPlot.cc\(${process},${runnumber},${nEvents},${save_tree}\)
 
-echo all done process the Fun4All_SingleParticlePi0.C, process_id: ${process}
+echo all done process the Fun4All_DisplayLegoPlot.C, process_id: ${process}

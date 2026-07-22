7deb52f20f8724470ede63b946c34ca490dffb3f
Ryotaro Koike <ryotaro@sphnxuser04.sdcc.bnl.gov>
2026-07-22 09:43:30 -0400

    corrected the path to nosplit bdt model

--- PhotonAnalysisTree/run_add_scores.sh
+++ PhotonAnalysisTree/run_add_scores.sh
@@ -2,13 +2,15 @@
 set -eo pipefail
 
 module_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) # the absolute path to the directory in which this script is placed.
-input_file=${1:?usage: run_add_scores.sh INPUT BDT_OUTPUT FINAL_OUTPUT [BDT_MODEL] [ONNX_MODEL]}
-split_bdt_output=${2:?usage: run_add_scores.sh INPUT BDT_OUTPUT FINAL_OUTPUT [BDT_MODEL] [ONNX_MODEL]}
-both_bdt_output=${3:?usage: run_add_scores.sh INPUT BDT_OUTPUT FINAL_OUTPUT [BDT_MODEL] [ONNX_MODEL]}
-final_output=${4:?usage: run_add_scores.sh INPUT BDT_OUTPUT FINAL_OUTPUT [BDT_MODEL] [ONNX_MODEL]}
-split_bdt_model=${5:-/sphenix/user/shuhangli/ppg12/FunWithxgboost/binned_models/model_base_v3E_split_single_tmva.root}
-nosplit_bdt_model=${6:-/sphenix/user/shuhangli/ppg12/FunWithxgboost/binned_models/model_base_v3E_split_single_tmva.root}
-onnx_model=${7:-$module_dir/models/best_model.onnx}
+input_file=${1:?usage: run_add_scores.sh INPUT SPLIT_BDT_OUTPUT BOTH_BDT_OUTPUT FINAL_OUTPUT [SPLIT_BDT_MODEL] [NOSPLIT_BDT_MODEL] [ONNX_MODEL]}
+split_bdt_output=${2:?usage: run_add_scores.sh INPUT SPLIT_BDT_OUTPUT BOTH_BDT_OUTPUT FINAL_OUTPUT [SPLIT_BDT_MODEL] [NOSPLIT_BDT_MODEL] [ONNX_MODEL]}
+both_bdt_output=${3:?usage: run_add_scores.sh INPUT SPLIT_BDT_OUTPUT BOTH_BDT_OUTPUT FINAL_OUTPUT [SPLIT_BDT_MODEL] [NOSPLIT_BDT_MODEL] [ONNX_MODEL]}
+split_score_output=${4:?usage: run_add_scores.sh INPUT SPLIT_BDT_OUTPUT BOTH_BDT_OUTPUT FINAL_OUTPUT [SPLIT_BDT_MODEL] [NOSPLIT_BDT_MODEL] [ONNX_MODEL]}
+final_output=${5:?usage: run_add_scores.sh INPUT SPLIT_BDT_OUTPUT BOTH_BDT_OUTPUT FINAL_OUTPUT [SPLIT_BDT_MODEL] [NOSPLIT_BDT_MODEL] [ONNX_MODEL]}
+split_bdt_model=${6:-/sphenix/user/shuhangli/ppg12/FunWithxgboost/binned_models/model_base_v3E_split_single_tmva.root}
+nosplit_bdt_model=${7:-/sphenix/user/shuhangli/ppg12/FunWithxgboost/binned_models/model_base_v3E_nosplit_single_tmva.root}
+split_onnx_model=${8:-$module_dir/models/best_model.onnx}
+nosplit_onnx_model=${9:-$module_dir/models/best_model.onnx}
 
 source /opt/sphenix/core/bin/sphenix_setup.sh -n ana
 export LD_LIBRARY_PATH="$module_dir/install/lib64:${LD_LIBRARY_PATH:-}"
@@ -20,8 +22,11 @@ root -l -b -q \
   -e ".L $module_dir/macro/add_nosplit_bdt.C" \
   -e "gSystem->Exit(add_nosplit_bdt(\"$split_bdt_output\",\"$both_bdt_output\",\"$nosplit_bdt_model\"));"
 root -l -b -q \
+  -e ".L $module_dir/macro/add_split_gamma_onnx.C" \
+  -e "gSystem->Exit(add_split_gamma_onnx(\"$both_bdt_output\",\"split_score_output\",\"$split_onnx_model\"));"
+root -l -b -q \
   -e ".L $module_dir/macro/add_nosplit_gamma_onnx.C" \
-  -e "gSystem->Exit(add_nosplit_gamma_onnx(\"$both_bdt_output\",\"$final_output\",\"$onnx_model\"));"
+  -e "gSystem->Exit(add_nosplit_gamma_onnx(\"$split_score_output\",\"$final_output\",\"$nosplit_onnx_model\"));"
 root -l -b -q \
   -e ".L $module_dir/macro/check_scored_tree.C" \
   -e "gSystem->Exit(check_scored_tree(\"$final_output\"));"
